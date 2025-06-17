5148436f57750b56e43b9c62a9124dca37d4e763
j-guimaraes <j.fmesquitaguimaraes@gmail.com>
2025-06-17 23:36:40 +0100

    Update app.py
    eliminei o ultimo fullfulment text para forçar a resposta estilo telegram

--- app.py
+++ app.py
@@ -77,7 +77,6 @@ def webhook():
         resposta += f"\n\n📊 A mostrar as {len(itens)} notícias mais recentes"
     
     return jsonify({
-        "fulfillmentText": resposta,
         "payload": {
             "telegram": {
                 "text": resposta,
